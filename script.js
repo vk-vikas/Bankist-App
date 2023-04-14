01a7370f2c9709158374792b7da10c2586af4b87
vikas kumar <vk.vikas3003@gmail.com>
2023-04-14 20:03:40 +0530

    loan feature implemented

--- script.js
+++ script.js
@@ -178,6 +178,21 @@ btnClose.addEventListener("click", function (e) {
   inputCloseUsername.value = inputClosePin.value = "";
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // add movement
+    currentAccount.movements.push(amount);
+    inputLoanAmount.value = "";
+    updateUI(currentAccount);
+  }
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
